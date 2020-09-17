ade525c5fba4e223175afb38bc9a921ae167c3d5
Sebastián <sebasbarrera981119@gmail.com>
2020-09-16 22:38:16 -0500

    The test of the stack and the queue has been added

---
+++ src/test/QueueTest.java
@@ -0,0 +1,149 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import dataStructure.Node;
+import dataStructure.Queue;
+
+class QueueTest {
+
+	private Queue<Integer> queue;
+	
+	private void setupStage0() {
+		queue = new Queue<>();
+	}
+	
+	private void setupStage1() {
+		setupStage0();
+		Node<Integer> n1 = new Node<>(532);
+		queue.offer(n1.getValue());
+	}
+	private void setupStage2() {
+		setupStage1();
+		Node<Integer> n2 = new Node<>(99472);
+		queue.offer(n2.getValue());
+		Node<Integer> n3 = new Node<>(4272942);
+		queue.offer(n3.getValue());
+	}
+	
+	@Test
+	void testOffer0() {
+		setupStage0();
+		int testValue = 75343;
+		queue.offer(new Node<>(testValue).getValue());
+		assertEquals(queue.peek(), testValue);
+		assertEquals(queue.getBack().getValue(), testValue);
+	}
+	
+	@Test
+	void testOffer1() {
+		setupStage1();
+		int testValue = 582935;
+		queue.offer(new Node<>(testValue).getValue());
+		assertEquals(queue.getBack().getValue(), testValue);
+	}
+	
+	@Test
+	void testOffer2() {
+		setupStage2();
+		int testValue = 2359926;
+		queue.offer(new Node<>(testValue).getValue());
+		assertEquals(queue.getBack().getValue(), testValue);
+	}
+	
+	@Test
+	void testPoll0() {
+		setupStage0();
+		queue.poll();
+		assertEquals(queue.peek(), null, "The queue is not empty");
+	}
+	
+	@Test
+	void testPoll1() {
+		setupStage1();
+		queue.poll();
+		assertEquals(queue.peek(), null, "The queue is not empty");
+	}
+	
+	@Test
+	void testPoll2() {
+		setupStage2();
+		queue.poll();
+		assertEquals(queue.peek(), 99472, "The value retorned is not the expected");
+	}
+	
+	@Test
+	void testIsEmpty0() {
+		setupStage0();
+		assertTrue(queue.isEmpty(), "The queue is not empty");
+	}
+	
+	@Test
+	void testIsEmpty1() {
+		setupStage1();
+		assertFalse(queue.isEmpty(), "The queue is empty");
+	}
+	
+	@Test
+	void testIsEmpty2() {
+		setupStage2();
+		assertFalse(queue.isEmpty(), "The queue is empty");
+	}
+	
+	@Test
+	void testPeek0() {
+		setupStage0();
+		assertEquals(queue.peek(), null, "There is something in the queue");
+	}
+	
+	@Test
+	void testPeek1() {
+		setupStage1();
+		assertEquals(queue.peek(), 532, "The value retorned is not the expected");
+	}
+	
+	@Test
+	void testPeek2() {
+		setupStage2();
+		assertEquals(queue.peek(), 532, "The value retorned is not the expected");
+	}
+	
+	@Test
+	void testSize0() {
+		setupStage0();
+		assertEquals(queue.size(), 0, "There is something in the queue");
+	}
+	
+	@Test
+	void testSize1() {
+		setupStage1();
+		assertEquals(queue.size(), 1, "The size of the queue is not 1");
+	}
+	
+	@Test
+	void testSize2() {
+		setupStage2();
+		assertEquals(queue.size(), 3, "The size of the queue is not 1");
+	}
+	
+	@Test
+	void testClear0() {
+		setupStage0();
+		queue.clear();
+		assertTrue(queue.isEmpty(), "The queue is not empty");
+	}
+	
+	@Test
+	void testClear1() {
+		setupStage1();
+		queue.clear();
+		assertTrue(queue.isEmpty(), "The queue is not empty");
+	}
+	
+	@Test
+	void testClear2() {
+		setupStage2();
+		queue.clear();
+		assertTrue(queue.isEmpty(), "The queue is not empty");
+	}
+}
---
+++ src/test/StackTest.java
@@ -0,0 +1,128 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import dataStructure.Node;
+import dataStructure.Stack;
+
+class StackTest {
+
+	private Stack<Integer> stack;
+	
+	private void setupStage0() {
+		stack = new Stack<>();
+	}
+	
+	private void setupStage1() {
+		setupStage0();
+		Node<Integer> n1 = new Node<>(532);
+		stack.push(n1.getValue());
+	}
+	private void setupStage2() {
+		setupStage1();
+		Node<Integer> n2 = new Node<>(99472);
+		stack.push(n2.getValue());
+		Node<Integer> n3 = new Node<>(4272942);
+		stack.push(n3.getValue());
+	}
+	
+	@Test
+	void testPush0() {
+		setupStage0();
+		int testValue = 75343;
+		stack.push(new Node<>(testValue).getValue());
+		assertEquals(stack.top(), testValue);
+	}
+	
+	@Test
+	void testPush1() {
+		setupStage1();
+		int testValue = 582935;
+		stack.push(new Node<>(testValue).getValue());
+		assertEquals(stack.top(), testValue);
+	}
+	
+	@Test
+	void testPush2() {
+		setupStage2();
+		int testValue = 2359926;
+		stack.push(new Node<>(testValue).getValue());
+		assertEquals(stack.top(), testValue);
+	}
+	
+	@Test
+	void testPop0() {
+		setupStage0();
+		stack.pop();
+		assertEquals(stack.top(), null, "The stack is not empty");
+	}
+	
+	@Test
+	void testPop1() {
+		setupStage1();
+		stack.pop();
+		assertEquals(stack.top(), null, "The stack is not empty");
+	}
+	
+	@Test
+	void testPop2() {
+		setupStage2();
+		stack.pop();
+		assertEquals(stack.top(), 99472, "The value retorned is not the expected");
+	}
+	
+	@Test
+	void testIsEmpty0() {
+		setupStage0();
+		assertTrue(stack.isEmpty(), "The stack is not empty");
+	}
+	
+	@Test
+	void testIsEmpty1() {
+		setupStage1();
+		assertFalse(stack.isEmpty(), "The stack is empty");
+	}
+	
+	@Test
+	void testIsEmpty2() {
+		setupStage2();
+		assertFalse(stack.isEmpty(), "The stack is empty");
+	}
+	
+	@Test
+	void testTop0() {
+		setupStage0();
+		assertEquals(stack.top(), null, "There is something in the stack");
+	}
+	
+	@Test
+	void testTop1() {
+		setupStage1();
+		assertEquals(stack.top(), 532, "The value retorned is not the expected");
+	}
+	
+	@Test
+	void testTop2() {
+		setupStage2();
+		assertEquals(stack.top(), 4272942, "The value retorned is not the expected");
+	}
+	
+	@Test
+	void testSize0() {
+		setupStage0();
+		assertEquals(stack.size(), 0, "There is something in the stack");
+	}
+	
+	@Test
+	void testSize1() {
+		setupStage1();
+		assertEquals(stack.size(), 1, "The size of the stack is not 1");
+	}
+	
+	@Test
+	void testSize2() {
+		setupStage2();
+		assertEquals(stack.size(), 3, "The size of the stack is not 1");
+	}
+
+}
