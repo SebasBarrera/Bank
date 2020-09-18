f8fba9a86812e2c706d5cef849fab55fdb77ae1e
Sebastián <sebasbarrera981119@gmail.com>
2020-09-18 12:29:55 -0500

    The data structures has been fixed

--- src/dataStructure/HashTable.java
+++ src/dataStructure/HashTable.java
@@ -2,10 +2,11 @@ package dataStructure;
 
 public class HashTable<K, V> implements IHashTable<K, V>{
 	
-	public final static int ARRAY_SIZE = 1000;
+	public final static int ARRAY_SIZE = 997;
 	 
 	private NodeH<K, V>[] nodes;
 	
+	@SuppressWarnings("unchecked")
 	public HashTable() {
 		nodes = (NodeH<K,V>[])new NodeH[ARRAY_SIZE];
 	}
@@ -18,16 +19,17 @@ public class HashTable<K, V> implements IHashTable<K, V>{
 		this.nodes = nodes;
 	}
 	
-	public int index(K key) {
+	public int hashFunction(K key) {
 		return key.hashCode() % ARRAY_SIZE;
 	}
 	
 	@Override
 	public void insert(K key, V value) {
-		NodeH<K, V> current = nodes[index(key)];
+		int position = hashFunction(key);
+		NodeH<K, V> current = nodes[position];
 		if (current == null) {
 			current = new NodeH<K, V>(key, value);
-			nodes[index(key)] = current;
+			nodes[position] = current;
 		} else {
 			boolean found = false;
 			while (current != null && !found) {
@@ -37,46 +39,47 @@ public class HashTable<K, V> implements IHashTable<K, V>{
 				}
 				current.getNext();
 			}
-			current = nodes[index(key)];
-			NodeH<K, V> newCurrent = new NodeH<K, V>(key, value);
-			current.setPrev(newCurrent);
-			newCurrent.setNext(current);
-			nodes[index(key)] = newCurrent;
+			if (!found) {
+				current = nodes[position];
+				NodeH<K, V> newCurrent = new NodeH<K, V>(key, value);
+				current.setPrev(newCurrent);
+				newCurrent.setNext(current);
+				nodes[position] = newCurrent;
+			}
 		}
 	}
 
 	@Override
 	public void delete(K key) {
-		NodeH<K, V> current = nodes[index(key)];
+		int position = hashFunction(key);
+		NodeH<K, V> current = nodes[position];
 		boolean found = false;
-		if (current != null) {
-			while (current != null && !found) {
-				if (current.getKey().equals(key)) {
-					found = true;
-					if (current.getNext() != null) {
-						current.getNext().setPrev(current.getPrev());
-					}
-					if (current.getPrev() != null) {
-						current.getPrev().setNext(current.getNext());
-					}
-					nodes[index(key)] = current.getNext(); 
+		while (current != null && !found) {
+			if (current.getKey().equals(key)) {
+				found = true;
+				if (current.getNext() != null) {
+					current.getNext().setPrev(current.getPrev());
 				}
-				current = current.getNext();
+				if (current.getPrev() != null) {
+					current.getPrev().setNext(current.getNext());
+				}
+				nodes[position] = current.getNext(); 
 			}
+			current = current.getNext();
 		}
 	}
 
 	@Override
 	public V getValue(K key) {
-		NodeH<K, V> newKey = nodes[index(key)];
+		NodeH<K, V> current = nodes[hashFunction(key)];
 		V value = null;
 		boolean found = false;
-		while (newKey != null && !found) {
-			if (newKey.getKey().equals(key)) {
+		while (current != null && !found) {
+			if (current.getKey().equals(key)) {
 				found = true;
-				value = newKey.getValue();
+				value = current.getValue();
 			}
-			newKey = newKey.getNext();
+			current = current.getNext();
 		}
 		return value;
 	}
@@ -84,7 +87,6 @@ public class HashTable<K, V> implements IHashTable<K, V>{
 	@Override
 	public V search(K key) {
 		return getValue(key);
-	}
-	
+	}	
 
 }
