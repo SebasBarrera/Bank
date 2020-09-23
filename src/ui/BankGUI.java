395b81fe4988201638087b54c1d39a10f1a36050
Alejandro Garcia <alejo.gar.122@gmail.com>
2020-09-23 15:38:01 -0500

    gui initializated

---
+++ src/ui/BankGUI.java
@@ -0,0 +1,17 @@
+package ui;
+
+import model.Controller;
+
+public class BankGUI {
+	private Controller control;
+	
+	public BankGUI() {
+		control = new Controller();
+		initialize();
+	}
+	
+	private void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,14 +1,33 @@
 package ui;
 
+import javafx.fxml.FXMLLoader;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Main extends Application{
+private BankGUI bankGUI;
+	
+	public Main() {
+		bankGUI = new BankGUI();
 	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		//TODO FXMLLoader fxmll = new FXMLLoader(getClass().getResource("InitialScreen.fxml"));
+		//fxmll.setController(bankGUI);
+		//Parent root = fxmll.load();
+		//root.getStylesheets().add(getClass().getResource("styleFirstScreen.css").toExternalForm());
+		
+		//Scene scene = new Scene(root);
+		//primaryStage.setScene(scene);
+		primaryStage.setTitle("Bank Application");
+		//primaryStage.setResizable(false);
+		primaryStage.show();
+	}
 }
