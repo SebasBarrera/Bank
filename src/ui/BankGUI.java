cd5d55bf7307b5c648a5d43b187ef6992868c316
Alejandro Garcia <alejo.gar.122@gmail.com>
2020-09-29 08:57:21 -0500

    changes added to gui

--- src/ui/BankGUI.java
+++ src/ui/BankGUI.java
@@ -1,8 +1,10 @@
 package ui;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import customExceptions.AreadyAddedIdException;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,11 +12,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import model.Controller;
@@ -23,8 +28,6 @@ import model.Person;
 public class BankGUI {
 	private Controller control;
 	private Stage extraStage;
-	@FXML
-	private AnchorPane clientInfo;
 	@FXML
 	private Label clientOptionsInfo;
 	@FXML
@@ -44,9 +47,9 @@ public class BankGUI {
     @FXML
     private TextField idField;
     @FXML
-    private Label row1;
+    private ListView<String> row1;
     @FXML
-    private Label row2;
+    private ListView<String> row2;
 	
 	public BankGUI() {
 		try {
@@ -58,41 +61,42 @@ public class BankGUI {
 	}
 	
 	@FXML
-	void searchClientNShowUpdateOptions(ActionEvent event) throws IOException {
+	void attendClient(MouseEvent event) throws IOException {
 		showInfoScreen();
 	}
 	
 	void showInfoScreen() throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("OptionsInfo.fxml"));
-
 		fxmlLoader.setController(this);
 		Parent infoPane = fxmlLoader.load();
 
-		clientInfo.getChildren().clear();
-		clientInfo.getChildren().add(infoPane);
-	}
-	
-	@FXML
-	void showPredeterminedInfoScreen(ActionEvent event) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("PredeterminatedInfoScreen.fxml"));
-
-		fxmlLoader.setController(this);
-		Parent infoPane = fxmlLoader.load();
-
-		clientInfo.getChildren().clear();
-		clientInfo.getChildren().add(infoPane);
+		Scene scene = new Scene(infoPane);
+		Stage stage = new Stage();
+		stage.setScene(scene);
+		stage.setTitle("Update client");
+		extraStage = stage;
+		stage.show();
 	}
 	
 	@FXML
 	void accountIsBeingCanceledAlert(ActionEvent event) {
-		Alert cancelAcc = new Alert(AlertType.WARNING);
+		Alert cancelAcc = new Alert(AlertType.CONFIRMATION);
 		cancelAcc.setTitle("U are trying to cancel an account");
 		cancelAcc.setHeaderText("ARE U SURE U WANT TO CANCEL THIS ACCOUNT?");
-		cancelAcc.showAndWait();
 		
-//		if(cancelAcc.isShowing()) {   tengo que verificar si cancela o acepta 
-//			TODO cancelar la cuenta
-//		}
+		Optional<ButtonType> result = cancelAcc.showAndWait();
+		if(result.get() == ButtonType.OK) {
+		     //TODO hay que cancelar la cuenta
+			Alert accCanceled = new Alert(AlertType.INFORMATION);
+			accCanceled.setTitle("Account canceled");
+			accCanceled.setHeaderText("The choosen account has been canceled");
+			accCanceled.showAndWait();
+		}else {
+			Alert accCanceled = new Alert(AlertType.INFORMATION);
+			accCanceled.setTitle("Account was not canceled");
+			accCanceled.setHeaderText("The choosen account has not been canceled");
+			accCanceled.showAndWait();
+		}
 	}
 	
 	@FXML
